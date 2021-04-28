8a59329ee2a24eaf051a668d26c2794b6edc9716
XuanDi <zerosky1943@gmail.com>
2021-04-28 15:02:42 +0800

    Update 2021-04-27.py

--- LeetCode/Python3/2021-04-27.py
+++ LeetCode/Python3/2021-04-27.py
@@ -1,3 +1,9 @@
+"""
+21038 / 21038 test cases passed.
+Status: Accepted
+Runtime: 72 ms
+Memory Usage: 14.4 MB
+"""
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         def rec(power: int, num: int):
@@ -8,3 +14,13 @@ class Solution:
             return False
         power, left = rec(0, n)
         return left == 1.0
+
+"""
+21038 / 21038 test cases passed.
+Status: Accepted
+Runtime: 64 ms
+Memory Usage: 13.9 MB
+"""
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 1162261467 % n == 0
