82504ba8fa760cdcd8ae5483563f59eca06f8092
XuanDi <zerosky1943@gmail.com>
2021-05-10 17:38:07 +0800

    Update examine.py

--- LeetCode/examine.py
+++ LeetCode/examine.py
@@ -2,25 +2,24 @@ import time
 import importlib
 import argparse
 
-TestSets = {
-    '2021-05-07': 'minDistance',
-    '2021-05-08': 'superpalindromesInRange',
-    '2021-05-09': 'isPossible',
-}
-
 
 def select_problem(category: str, filename: str):
     mod = importlib.import_module(f'{category}.{filename}')
-    solution = getattr(mod, 'Solution')
+    solution = getattr(mod, 'Solution')()
     testcases = getattr(mod, 'testcases')
     return solution, testcases
 
 
-def examine(cls, method: str, testcases: dict) -> None:
-    sol = cls()
+def find_method(solution) -> str:
+    for m in dir(solution):
+        if not m.startswith('_'):
+            return m
+
+
+def examine(sol, method: str, testcases: dict) -> None:
     cost = 0
     for idx, testcase in testcases.items():
-        print(f'Question {idx}\n')
+        print(f'Question {idx}')
         inputs, ans = testcase
         print(f'Inputs: {inputs}')
         t0 = time.time()
@@ -29,14 +28,14 @@ def examine(cls, method: str, testcases: dict) -> None:
         cost += dt
         print(f'Cost: {dt} ms')
         if ret != ans:
-            print('------------------------------')
             print(
+                f'------------------------------\n'
                 f'*WrongAnswer index {idx}\n'
                 f'answer: {ans}\n'
                 f'yours: {ret}\n'
             )
             break
-        print('PASS')
+        print('PASS\n')
     else:
         n_cases = len(testcases)
         print(
@@ -51,9 +50,9 @@ def main(args):
     category = 'python3'
     filename = args.solution
     solution, testcases = select_problem(category, filename)
-    method = TestSets[filename]
+    method = find_method(solution)
     examine(
-        cls=solution,
+        sol=solution,
         method=method,
         testcases=testcases,
     )
