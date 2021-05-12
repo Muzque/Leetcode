be7e9850db5ed5fc11db8302e348e1d71671eca5
XuanDi <zerosky1943@gmail.com>
2021-05-12 16:11:06 +0800

    Update 2021-05-08.py

--- LeetCode/Python3/2021-05-08.py
+++ LeetCode/Python3/2021-05-08.py
@@ -2,12 +2,6 @@
 Super Palindromes
 https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3736/
 """
-"""
-49 / 49 test cases passed.
-Status: Accepted
-Runtime: 1104 ms
-Memory Usage: 40.6 MB
-"""
 testcases = {
     '1': (["4", "1000"], 4),
     '2': (["1", "2"], 1),
@@ -19,8 +13,9 @@ testcases = {
     '8': (['6000000', '70000000'], 0),
     '9': (['10000000', '50000000'], 0),
     '10': (['400000000', '500000000'], 2),
+    '11': (['111', '231'], 1),
+    '12': (['1', '999999999999999999'], 70),
 }
-
 
 import math
 
@@ -44,7 +39,16 @@ class Solution1:
         return cnt
 
 
-class Solution:
+"""
+49 / 49 test cases passed.
+Status: Accepted
+Runtime: 1104 ms
+Memory Usage: 40.6 MB
+"""
+
+
+# Avg. 102.99
+class Solution2:
 
     def _get_possibility(self, bot, top):
         left, right = len(str(bot)), len(str(top))
@@ -83,3 +87,76 @@ class Solution:
             if right >= nn >= left and self._is_palindrome(nn):
                 cnt += 1
         return cnt
+
+
+"""
+49 / 49 test cases passed.
+Status: Accepted
+Runtime: 1112 ms
+Memory Usage: 40.7 MB
+"""
+
+
+# Avg cost: 98.84
+class Solution3:
+
+    def _get_possibility(self, bot, top):
+        left, right = len(str(bot)), len(str(top))
+        even = [['00', '11', '22', '33', '44', '55', '66', '77', '88', '99']]
+        while len(even[-1][0])+2 <= right:
+            tmp = [f'{i}{a}{i}' for a in even[-1] for i in range(10)]
+            even.append(tmp)
+        odd = [['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']]
+        while len(odd[-1][0])+2 <= right:
+            tmp = [f'{i}{a}{i}' for a in odd[-1] for i in range(10)]
+            odd.append(tmp)
+        ret = []
+        for arr in even:
+            ret += [int(n) for n in arr]
+        for arr in odd:
+            ret += [int(n) for n in arr]
+        return ret
+
+    def _is_palindrome(self, num: int) -> bool:
+        s = str(num)
+        h = len(s)
+        mid = h // 2
+        return s[:mid] == s[:h-mid-1:-1]
+
+    def superpalindromesInRange(self, left: str, right: str) -> int:
+        bot = math.ceil(int(left) ** 0.5)
+        top = math.floor(int(right) ** 0.5)
+        arr = self._get_possibility(bot, top)
+        cnt = 0
+        left, right = int(left), int(right)
+        for n in arr:
+            nn = n ** 2
+            if right >= nn >= left and self._is_palindrome(nn):
+                cnt += 1
+        return cnt
+
+
+"""
+49 / 49 test cases passed.
+Status: Accepted
+Runtime: 88 ms
+Memory Usage: 14.4 MB
+"""
+
+from itertools import product
+
+
+# Avg cost: 25.30
+class Solution:
+    def superpalindromesInRange(self, left: str, right: str) -> int:
+        left, right = int(left), int(right)
+        seen = set()
+        for d in product(('0', '1', '2', '3'), repeat=5):
+            num = "".join(d).rstrip('0')
+            if not num:
+                continue
+            for n in (num[::-1] + num, num[::-1] + num[1:]):
+                x = int(n) ** 2
+                if x not in seen and left <= x <= right:
+                    seen.add(x)
+        return sum(str(x) == str(x)[::-1] for x in seen)
