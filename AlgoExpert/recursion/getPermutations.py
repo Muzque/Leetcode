ad03225138ab791c9c5be8c517f238a0d21d50e8
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-10 16:55:24 +0800

    tmp(ae/recursion): getPermutations

---
+++ AlgoExpert/recursion/getPermutations.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+testcases = [
+    {
+        'input': {
+            'array': [1, 2, 3]
+        },
+        'output': [
+            [1, 2, 3],
+            [1, 3, 2],
+            [2, 1, 3],
+            [2, 3, 1],
+            [3, 1, 2],
+            [3, 2, 1],
+        ],
+    },
+]
+
+from lib import run_tests
+
+
+def getPermutations(array):
+    ret = []
+
+    return ret
+
+
+def main():
+    run_tests(
+        testcases=testcases,
+        function=getPermutations,
+    )
