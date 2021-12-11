3992c0807b3bcba2e6d79f8cd0b392349759ddf2
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-11 22:21:50 +0800

    feat(ae/linkedList): removeDuplicatesFromLinkedList

--- AlgoExpert/lib.py
+++ AlgoExpert/lib.py
@@ -37,8 +37,25 @@ def gen_tree_root(nodes, root='1') -> BinaryTree:
     return edges[root]
 
 
-def display_test_result(index, case, result):
-    is_passed = result == case['output']
+def linkedList2array(linked_list):
+    array = []
+    while linked_list is not None:
+        array.append(linked_list.value)
+        linked_list = linked_list.next
+    return array
+
+
+def check_result(mode: str, a, b):
+    if mode == 'default':
+        return a == b
+    if mode == 'linkedList':
+        tmp_a = linkedList2array(a)
+        tmp_b = linkedList2array(b)
+        return tmp_a == tmp_b
+
+
+def display_test_result(index, case, result, mode: str):
+    is_passed = check_result(mode, case['output'], result)
     result_string = 'Pass!' if is_passed else 'Fail!'
     print(f'Question {index}:')
     print(f'Test result: {result_string}')
@@ -49,7 +66,25 @@ def display_test_result(index, case, result):
     print('-' * 87)
 
 
-def run_tests(testcases, function):
+def run_tests(testcases, function, mode='default'):
+    print(f'Checker mode: {mode}')
     for idx, tc in enumerate(testcases, 1):
         ret = function(**tc['input'])
-        display_test_result(idx, tc, ret)
+        display_test_result(idx, tc, ret, mode)
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def gen_linked_list(nodes, head='1'):
+    vertices = {}
+    for obj in nodes:
+        vertices[obj['id']] = LinkedList(value=obj['value'])
+    for obj in nodes:
+        vertex = vertices[obj['id']]
+        if obj['next'] is not None:
+            vertex.next = vertices[obj['next']]
+    return vertices[head]
---
+++ AlgoExpert/linked_lists/removeDuplicatesFromLinkedList.py
@@ -0,0 +1,72 @@
+testcases = [
+    {
+        'input': {
+            "linkedList": {
+                "head": "1",
+                "nodes": [
+                    {"id": "1", "next": "1-2", "value": 1},
+                    {"id": "1-2", "next": "1-3", "value": 1},
+                    {"id": "1-3", "next": "2", "value": 1},
+                    {"id": "2", "next": "3", "value": 3},
+                    {"id": "3", "next": "3-2", "value": 4},
+                    {"id": "3-2", "next": "3-3", "value": 4},
+                    {"id": "3-3", "next": "4", "value": 4},
+                    {"id": "4", "next": "5", "value": 5},
+                    {"id": "5", "next": "5-2", "value": 6},
+                    {"id": "5-2", "next": None, "value": 6}
+                ]
+            }
+        },
+        'output': {
+            "head": "1",
+            "nodes": [
+                {"id": "1", "next": "3", "value": 1},
+                {"id": "3", "next": "4", "value": 3},
+                {"id": "4", "next": "5", "value": 4},
+                {"id": "5", "next": "6", "value": 5},
+                {"id": "6", "next": None, "value": 6}
+            ]
+        }
+    },
+]
+
+from lib import gen_linked_list, run_tests
+
+
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    head = linkedList
+    tmp = linkedList.next
+    while tmp is not None:
+        if linkedList.value == tmp.value:
+            linkedList.next = tmp.next
+            tmp = tmp.next
+        else:
+            linkedList = tmp
+            tmp = linkedList.next
+    return head
+
+
+def main():
+    wrapped_cases = []
+    key = 'linkedList'
+    for tc in testcases:
+        head = gen_linked_list(**tc['input'][key])
+        head_ans = gen_linked_list(**tc['output'])
+        wrapped_cases.append({
+            'input': {
+                key: head,
+            },
+            'output': head_ans,
+        })
+    run_tests(
+        testcases=wrapped_cases,
+        function=removeDuplicatesFromLinkedList,
+        mode=key,
+    )
