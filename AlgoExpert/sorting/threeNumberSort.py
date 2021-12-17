e153507aa5afcda1fd3a046872c9d0dc4cec3dec
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-17 16:12:03 +0800

    enhance(ae/sorting): threeNumberSort

--- AlgoExpert/sorting/threeNumberSort.py
+++ AlgoExpert/sorting/threeNumberSort.py
@@ -18,6 +18,7 @@ def main():
     )
 
 
+"""
 def threeNumberSort(array, order):
     for i in range(1, len(array)):
         j = i
@@ -26,4 +27,19 @@ def threeNumberSort(array, order):
                 (array[j] == order[1] and array[j-1] == order[2])):
             array[j], array[j-1] = array[j-1], array[j]
             j -= 1
+    return array
+"""
+
+
+def threeNumberSort(array, order):
+    counts = [0, 0, 0]
+    for n in array:
+        idx = order.index(n)
+        counts[idx] += 1
+    for i in range(3):
+        num = order[i]
+        left = sum(counts[:i])
+        right = left + counts[i]
+        for j in range(left, right):
+            array[j] = num
     return array
