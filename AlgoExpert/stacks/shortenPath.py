c7c4b71878d12b8031caae60fd46a9f0155c04fb
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-23 14:45:07 +0800

    refactor(ae/stacks): shortenPath

--- AlgoExpert/stacks/shortenPath.py
+++ AlgoExpert/stacks/shortenPath.py
@@ -42,6 +42,7 @@ def main():
     )
 
 
+"""
 def shortenPath(path):
     stack = []
     is_abs = path[0] == '/'
@@ -56,3 +57,20 @@ def shortenPath(path):
             stack.append(p)
     result = '/'.join(stack) or '/'
     return '/' + result if is_abs and result[0] != '/' else result
+"""
+
+
+def shortenPath(path):
+    stack = []
+    if path[0] == '/':
+        stack.append('')
+    for p in path.split('/'):
+        if (stack and stack[-1] == '' and p == '..') or p in ('.', ''):
+            continue
+        if len(stack) == 0:
+            stack.append(p)
+        elif p == '..' and stack[-1] != '..':
+            stack.pop()
+        else:
+            stack.append(p)
+    return '/'.join(stack) or '/'
