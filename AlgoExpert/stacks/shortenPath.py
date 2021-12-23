f8ffd6b602c4dd874ea2922cd53b695e0cb64975
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-23 14:37:30 +0800

    feat(ae/stacks): shortenPath

---
+++ AlgoExpert/stacks/shortenPath.py
@@ -0,0 +1,58 @@
+testcases = [
+    {
+        'input': {
+            "path": "/foo/../test/../test/../foo//bar/./baz"
+        },
+        'output': '/foo/bar/baz',
+    },
+    {
+        'input': {
+            "path": "../../foo/../../bar/baz"
+        },
+        'output': '../../../bar/baz',
+    },
+    {
+        'input': {
+            "path": "/../../foo/bar/baz"
+        },
+        'output': '/foo/bar/baz',
+    },
+    {
+        'input': {
+            "path": "/"
+        },
+        'output': '/',
+    },
+    {
+        'input': {
+            "path": "./.."
+        },
+        'output': '..'
+    },
+]
+
+
+from lib import run_tests
+
+
+def main():
+    run_tests(
+        testcases=testcases,
+        function=shortenPath,
+    )
+
+
+def shortenPath(path):
+    stack = []
+    is_abs = path[0] == '/'
+    for p in path.split('/'):
+        if (is_abs and len(stack) == 0 and p == '..') or p in ('.', ''):
+            continue
+        if len(stack) == 0:
+            stack.append(p)
+        elif p == '..' and stack[-1] != '..':
+            stack.pop()
+        else:
+            stack.append(p)
+    result = '/'.join(stack) or '/'
+    return '/' + result if is_abs and result[0] != '/' else result
