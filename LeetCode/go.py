9407115bdad0f5a1eecfd9a3db4def6bb78787cc
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-12-27 13:39:39 +0800

    enhance(lc): 2121

---
+++ LeetCode/go.py
@@ -0,0 +1,37 @@
+import os
+import importlib
+import argparse
+from lib import timeit
+
+
+class Runner:
+    def __init__(self, question: str):
+        self.question = question.rstrip('.py')
+        self.func = self.load()
+
+    def load(self):
+        module_path = '.'.join(self.question.split(os.sep))
+        mod = importlib.import_module(module_path)
+        return getattr(mod, 'main')
+
+    @timeit
+    def go(self):
+        self.func()
+
+
+def main(args):
+    r = Runner(args.question)
+    r.go()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run test")
+    parser.add_argument(
+        "-q",
+        "--question",
+        type=str,
+        help="select question",
+        dest="question",
+        required=True,
+    )
+    main(parser.parse_args())
